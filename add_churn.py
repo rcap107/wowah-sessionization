2502823b17ce448d2ad7fed6f658fb132b9ff386
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2026-04-23 15:27:35 +0200

    fixing churn generation to remove unrealistic entries

--- add_churn.py
+++ add_churn.py
@@ -5,23 +5,23 @@ The idea is that, given a month, we want to use all the data up to the last day
 of the previous month to predict whether a user will churn in the next month.
 
 So, if we are in the month of April, we want to use all the data up to March 31st
-to predict whether a user will churn in May. We want to predict May because we 
-might want to take action in April to prevent the user from churning in May. 
+to predict whether a user will churn in May. We want to predict May because we
+might want to take action in April to prevent the user from churning in May.
 
 To do this, we need to create a dataset that has one row per user per month,
-and a column that indicates whether the user has played in that month or not. 
+and a column that indicates whether the user has played in that month or not.
 
 We need then to create a dataset with all the months in the range we have data for,
-then we need to do the cross product with the unique characters. 
-Now we want to prepare the data we have so that the "has played" colum is True 
+then we need to do the cross product with the unique characters.
+Now we want to prepare the data we have so that the "has played" colum is True
 if the user has played in that month, and False otherwise.
 
-We can do this by finding all the unique combinations of user and month in the 
+We can do this by finding all the unique combinations of user and month in the
 original dataset, and then doing a left join with the "user month" dataset.
 Any null values in the "has played" column will be filled with False, indicating
 that the user did not play in that month.
 
-This is the "churn" dataset, which we can then use to train a model. 
+This is the "churn" dataset, which we can then use to train a model.
 """
 
 # %%
@@ -30,6 +30,9 @@ import polars as pl
 # %%
 # Load the dataset
 df = pl.scan_parquet("data/wowah_data_raw.parquet")
+df = df.with_columns(first_month=pl.col("timestamp").dt.truncate("1mo").min().over("char"))
+
+
 # %%
 def make_user_month(df):
     months = pl.datetime_range(
@@ -69,16 +72,42 @@ data = make_data(df)
 def add_churn(user_month, data):
     df_with_user_month = (
         user_month.join(
-            data.select("char", "month", "has_played"), on=["char", "month"], how="left"
+            data.select(
+                "char",
+                "month",
+                "has_played",
+                "first_month",
+            ),
+            on=["char", "month"],
+            how="left",
         )
         .with_columns(pl.col("has_played").fill_null(False))
-        .select("char", "month", "has_played")
+        .select(
+            "char",
+            "month",
+            "has_played",
+            "first_month",
+        )
     )
     return df_with_user_month
+
+def remove_unrealistic_entries(churn_data, data):
+    # Remove entries where the month is before the first month seen for the character
+    churn_data = churn_data.join(
+        data.select("char", "first_month").unique(), on="char", how="left"
+    ).filter(pl.col("month") >= pl.col("first_month_right")).drop("first_month").select(
+        pl.col("char"),
+        pl.col("month"),
+        pl.col("has_played"),
+        pl.col("first_month_right").alias("first_month"),
+    )
+    return churn_data    
+    
 
 
 # %%
 churn_data = add_churn(user_month, data)
+churn_data = remove_unrealistic_entries(churn_data, data)
 # %%
 churn_data.collect().write_parquet("data/wowah_churn_data.parquet")
 # %%
