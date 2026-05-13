0caeb0cf2f8f7ba09ab277186cf329d046662870
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2026-05-13 14:37:47 +0200

    more work on adding features

--- adding_features.py
+++ adding_features.py
@@ -2,11 +2,9 @@
 # In this script I am testing the features that I can add to the historical data
 
 import polars as pl
+import datetime
 import skrub
 from src.utils import (
-    add_session_features,
-    add_char_features,
-    add_aggregated_features,
     sample_by_user,
 )
 
@@ -21,7 +19,12 @@ df = df.with_columns(guild=pl.col("guild").replace(-1, None))
 # %%
 df
 # %%
-target_month = pl.datetime(2008, 2, 1)
+# Here I define the target month, i.e., the month for which I want to predict churn.
+# The cutoff month is the month before the target month: the idea is that during
+# the "cutoff month" we can take some action to prevent churn in the "target month".
+# I then filter the historical data to only include data before the cutoff month.
+target_month = datetime.datetime(2008, 6, 1)
+cutoff_month = pl.Series([target_month]).dt.offset_by("-1mo").first()
 historical_data = df.filter(pl.col("timestamp") < target_month)
 historical_data = historical_data.with_columns(
     month=pl.col("timestamp").dt.truncate("1mo")
@@ -68,23 +71,91 @@ historical_data_with_sessions = session_encoder.fit_transform(historical_data)
 
 # %%
 def add_fixed_features(df):
-    return df.select("char", "race", "charclass")
+    return df.select("char", "race", "charclass").unique("char")
 
 
 query_data = query_data.join(add_fixed_features(historical_data), on="char", how="left")
 query_data
 # %%
-_ = historical_data_with_sessions.with_columns(
-    pl.col("timestamp").first().over("timestamp_session_id").alias("session_start"),
-    pl.col("timestamp").last().over("timestamp_session_id").alias("session_end"),
-).with_columns(
-    session_duration=pl.col("session_end") - pl.col("session_start"),
-)
-session_bounds = _.select(
-    "char", "session_start", "session_end", "timestamp_session_id"
-).unique("timestamp_session_id")
+# Adding the session start and end to find the session duration
+# Sessions that end within a single heartbeat have the same start and end, thus
+# duration = 0. I will replace those with a duration of 1 minute so that the
+# total logged time over a month is not 0. This is useful to distinguish between
+# players that never logged in and players that logged in but had very short sessions.
+
+
+def get_session_duration():
+    _ = (
+        historical_data_with_sessions.with_columns(
+            pl.col("timestamp")
+            .first()
+            .over("timestamp_session_id")
+            .alias("session_start"),
+            pl.col("timestamp")
+            .last()
+            .over("timestamp_session_id")
+            .alias("session_end"),
+        )
+        .with_columns(
+            (pl.col("session_end") - pl.col("session_start")).alias("session_duration"),
+        )
+        .with_columns(
+            pl.when(pl.col("session_duration") == pl.duration(microseconds=0))
+            .then(pl.duration(minutes=1))
+            .otherwise(pl.col("session_duration"))
+            .alias("session_duration")
+        )
+    )
+    return _
+
+
+hist_session_duration = get_session_duration()
+hist_session_duration
+
 # %%
-_.unique("timestamp_session_id").group_by("char").agg(
-    pl.col("session_duration").sum().alias("total_session_duration"),
-    pl.col("session_duration").mean().alias("avg_session_duration"),
-).sort("total_session_duration", descending=True)
+historical_duration = (
+    hist_session_duration.unique("timestamp_session_id")
+    .group_by("char")
+    .agg(
+        pl.col("session_duration").sum().alias("hist_total_session_duration"),
+        pl.col("session_duration").mean().alias("hist_avg_session_duration"),
+    )
+    .sort("hist_total_session_duration", descending=True)
+)
+# %%
+def add_session_features(df):
+    session_duration = (
+        hist_session_duration.unique("timestamp_session_id")
+        .group_by("char")
+        .agg(
+            pl.col("session_duration").sum().alias("hist_total_session_duration"),
+            pl.col("session_duration").mean().alias("hist_avg_session_duration"),
+            pl.col("session_duration").count().alias("hist_num_sessions"),
+        )
+    )
+    monthly_duration = (
+        hist_session_duration.unique("timestamp_session_id")
+        .group_by("char", "month")
+        .agg(
+            pl.col("session_duration").sum().alias("monthly_total_session_duration"),
+            pl.col("session_duration").mean().alias("monthly_avg_session_duration"),
+            pl.col("session_duration").count().alias("monthly_num_sessions"),
+        )
+    )
+    df = df.join(session_duration, on="char", how="left")
+    df = (
+        df.with_columns(previous_month=pl.col("month").dt.offset_by("-1mo"))
+        .join(
+            monthly_duration,
+            left_on=["char", "previous_month"],
+            right_on=["char", "month"],
+            how="left",
+        )
+        .drop("previous_month")
+    )
+    return df
+
+
+add_session_features(query_data)
+
+# %%
